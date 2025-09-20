4820b714687468f907ed2c313d4c4010aaad4844
andrius <andrius.korsakas@createfuture.com>
2025-09-20 17:33:17 +0100

    Removed unused python files and amended playwright file.
    Playwright python file now executes the task based on the prompt without interactive session.

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from playwright-agentic-tests!")
-
-
-if __name__ == "__main__":
-    main()
--- playwright_agent.py
+++ playwright_agent.py
@@ -4,12 +4,19 @@ import asyncio
 fast = FastAgent("Playwright MCP PoC")
 
 @fast.agent(
-    instruction="Go to the Playwright website and click on the 'Fixtures' link in the sidebar.",
+    instruction="You are a web automation testing agent. Use Playwright MCP server to interact with web pages to perform testing tasks.",
     servers=["playwright"],
 )
+
+async def test_webapp(usecases: str):
+    async with fast.run() as agent:
+        result = await agent.send(f"Test this web application: {usecases}")
+        return result
+
 async def main():
-  async with fast.run() as agent:
-    await agent()
+    usecases = "Go to wood mackenzie web app. Find report and add the report to the cart."
+    result = await test_webapp(usecases)
+    print("Test Result:", result)
 
 if __name__ == "__main__":
     asyncio.run(main())
