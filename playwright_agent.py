2a3f14d5c9f86c70a3ff8715979ab150b7991809
andrius <andrius.korsakas@createfuture.com>
2025-10-21 16:52:23 +0100

    Changed prompt to save traces separately

--- playwright_agent.py
+++ playwright_agent.py
@@ -5,7 +5,7 @@ import asyncio
 fast = FastAgent("Playwright MCP PoC", quiet=True)
 
 @fast.agent(
-    instruction="You are a web automation testing agent. Use Playwright MCP server to interact with web app. Execute test tasks provided using BDD/Gherkin format. Always take screenshot at the end of each bdd scenario and store it as {filename}.png under ./artifacts directory. Before and after each bdd scenario start browser_start_tracing and stop browser_stop_tracing then save it to ./artifacts/{filename}/ directories. Test report should be in the markdown format and saved as {filename}.md in the ./artifacts directory",
+    instruction="You are a web automation testing agent. Your task is to run test based on BDD/Gherkin prompt. Always take screenshot at the end of each bdd scenario and store it as {filename}.png. Before each test run browser_start_tracing and after test run browser_stop_tracing, save it to ./artifacts/{filename}/ directory. Traces need to be saved in each own {filename}-trace directory and not mix up. Test report should be in the markdown format and saved as {filename}.md in the ./artifacts directory",
     servers=["playwright"]
 )
 
